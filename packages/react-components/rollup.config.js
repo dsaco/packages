2b78ea95e021f0526c89b40e40fb825eff5cdf53
dsaco <2363140194@qq.com>
2023-07-26 00:23:24 +0800

    Update rollup.config.js

--- packages/react-components/rollup.config.js
+++ packages/react-components/rollup.config.js
@@ -10,59 +10,97 @@ import babel, { getBabelOutputPlugin } from '@rollup/plugin-babel';
 
 const input = [
   './src/components/index.ts',
-  './src/components/Button/index.ts',
+  // './src/components/Button/index.ts',
+];
+const external = [
+  'react',
+  'react-dom',
+  'styled-components',
+  '@react-spring/web',
+  '@dsaco/utils',
+  /@babel\/runtime/,
 ];
 
-export default {
-  input,
-  output: [
-    {
+export default [
+  {
+    input,
+    output: {
       dir: 'es',
       format: 'es',
       preserveModules: true,
-      plugins: [
-        getBabelOutputPlugin({
-          presets: [['@babel/preset-env']],
-          plugins: [['@babel/plugin-transform-runtime']],
-        }),
-      ],
+      // plugins: [
+      //   getBabelOutputPlugin({
+      //     presets: [['@babel/preset-env']],
+      //     plugins: [['@babel/plugin-transform-runtime']],
+      //   }),
+      // ],
     },
-    {
+    plugins: [
+      resolve({
+        extensions: ['.ts', '.tsx'],
+      }),
+      json(),
+      commonjs(),
+      typescript({
+        tsconfig: 'tsconfig.json',
+        tsconfigOverride: {
+          compilerOptions: {
+            target: 'ES5',
+            jsx: 'react',
+            // esModuleInterop: true,
+          },
+          include: input,
+        },
+      }),
+    ],
+    external,
+  },
+  {
+    input,
+    output: {
       dir: 'lib',
       format: 'cjs',
       preserveModules: true,
-      plugins: [
-        getBabelOutputPlugin({
-          presets: ['@babel/preset-env'],
-          plugins: [['@babel/plugin-transform-runtime']],
-        }),
-      ],
+      // plugins: [
+      //   getBabelOutputPlugin({
+      //     presets: ['@babel/preset-env'],
+      //     plugins: [['@babel/plugin-transform-runtime']],
+      //   }),
+      // ],
     },
-  ],
-  plugins: [
-    resolve({
-      extensions: ['.ts', '.tsx'],
-    }),
-    json(),
-    commonjs(),
-    typescript({
-      tsconfig: 'tsconfig.json',
-      tsconfigOverride: {
-        compilerOptions: {
-          // target: 'ES',
-          jsx: 'react',
-          // esModuleInterop: true,
+    plugins: [
+      resolve({
+        extensions: ['.ts', '.tsx'],
+      }),
+      json(),
+      commonjs(),
+      typescript({
+        tsconfig: 'tsconfig.json',
+        tsconfigOverride: {
+          compilerOptions: {
+            target: 'ES5',
+            jsx: 'react',
+            // esModuleInterop: true,
+          },
+          include: input,
         },
-        include: input,
-      },
-    }),
-  ],
-  external: [
-    'react',
-    'react-dom',
-    'styled-components',
-    '@react-spring/web',
-    '@dsaco/utils',
-    /@babel\/runtime/,
-  ],
-};
+      }),
+      // babel({
+      //   babelHelpers: 'runtime',
+      //   presets: [
+      //     [
+      //       '@babel/preset-env',
+      //       {
+      //         targets: 'defaults',
+      //         // targets: {
+      //         //   esmodules: false,
+      //         // },
+      //       },
+      //     ],
+      //   ],
+      //   plugins: ['@babel/plugin-transform-runtime'],
+      // }),
+    ],
+    external,
+  },
+];
